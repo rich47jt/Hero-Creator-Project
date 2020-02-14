052d5160effcb5bbb30d7f54c69fe07aaa34612e
unknown <theusjt49@gmail.com>
2020-02-14 14:01:48 -0600

    created new class in models folder for superhero with specifiedproperties from user story.

---
+++ Project SuperHero Creator/Models/SuperHeroes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_SuperHero_Creator.Models
+{
+    public class SuperHeroes
+    {
+        public int sI
+    }
+}
